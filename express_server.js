223d170e36d5961af3b37efa76b14b7c942a85e5
A <awongpro7@gmail.com>
2021-03-16 20:50:52 +0000

    Adding latest updates to express server

--- express_server.js
+++ express_server.js
@@ -27,5 +27,10 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("urls_index1", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
