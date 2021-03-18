906cbc43ba42ae355229326979a809f6060befa4
A <awongpro7@gmail.com>
2021-03-18 23:26:24 +0000

    Updated express_server.js file with new login page route

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+const { isExistingUser } = require(`./helperFunctions`)
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -13,6 +14,26 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// users Object
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+// console.log(users)
+
+
+
+
 
 const randomString = function() {
   let r = Math.random().toString(36).substring(7);
@@ -36,17 +57,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user:users[req.cookies.userID] };
   res.render("urls_index1", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
-  res.render("urls_new");
+  const templateVars = { user:users[req.cookies.userID] };
+  res.render("urls_new", templateVars );
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user:users[req.cookies.userID] };
   res.render("urls_show", templateVars);
 });
 
@@ -58,8 +79,20 @@ app.get("/urls/:shortURL", (req, res) => {
 
 // Not sure if this is needed? Correct?
 app.get("/urls/:longURL", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user:users[req.cookies.userID] };
   res.render("urls_show/:longURL", templateVars);
+});
+
+// Route for register
+app.get("/register", (req, res) => {
+  const templateVars = { user:users[req.cookies.userID] };
+  res.render("register", templateVars);
+});
+
+// Route new login page
+app.get("/login2", (req, res) => {
+  const templateVars = { user:users[req.cookies.userID] };
+  res.render("login2", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -104,7 +137,50 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   // console.log(req.cookies);
   // const templateVars = { username: req.cookies.username }
-  const username = req.body.username
-  res.clearCookie('username')
+  res.clearCookie('userID')
   res.redirect("/urls");
+});
+
+// Route for handling registration form data (POST)
+app.post("/register", (req, res) => {
+  
+  // Set up required info
+  console.log("req.body", req.body)
+  const email = req.body.email
+  const password = req.body.password
+
+  // If the e-mail or password are empty strings, send response with 400 status code
+  if ((!email) || (!password)) {
+    res.status(400).send("Email and password cannot be blank");
+    return;
+  }
+  
+  if (isExistingUser(email, users)) { 
+  // If someone tries registering with email already in users object, send response with 400 status code 
+    res.status(400).send("User already exists");
+    return;
+  }
+  
+  // Create random user ID
+  let randomUserID = randomString();
+  console.log("randomUserID", randomUserID)
+  
+  // Create new user object SOMETHING IS WRONG HERE
+  let newUser = { id: randomUserID, email: email, password: password }
+  console.log("newUser", newUser)
+
+  // Add new user to global users object?
+  users[randomUserID] = newUser
+
+  // Set a user_id cookie containing the user's newly generated ID
+  res.cookie('userID', randomUserID)
+
+  // Test that the users object is properly being appended to
+  console.log(users)
+
+  // Set conditionals to handle registration errors
+
+  // Redirect the user to the /urls page.
+  res.redirect("/urls");
+
 });
